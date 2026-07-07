b32b470157ac21c1d2259845e4c3304142a10e5f
NKWallixYT <wallisch.niklas@gmail.com>
2026-07-07 23:16:31 +0200

    Update StoryHub main menu design

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,15 @@ import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
+
 export class App {
+  searchOpen = false;
+  toggleSearch() {
+    this.searchOpen = !this.searchOpen;
+  }
   protected readonly title = signal('NKWallix Storyhub');
 }
